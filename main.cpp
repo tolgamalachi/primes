1d6380e3ca62f889c2c71987debd7960c7e9d7da
Tolga Aydemir <tolga.aydemir@fh-bielefeld.de>
2017-02-21 18:15:50 +0100

    kleine aenderung bzgl der ausgabe

--- main.cpp
+++ main.cpp
@@ -7,16 +7,22 @@ using namespace std;
 
 int main() {
     time_t start = time(nullptr);
-    int max = 999999999;
+    int max = 9999999;
+    int primes = 0;
 
 
     for (int i = 0; i < max; i++) {
-        Prime::IsPrime(i);
+        if (Prime::IsPrime(i)) {
+            primes++;
+        }
     }
 
     time_t end = time(nullptr);
-    cout << "Dauer in Sekunden um die Zahlen 0 bis " << max << " auf ihre Primeigenschaft hin zu überprüfen: " << end - start
+    cout << "Dauer in Sekunden um die Zahlen 0 bis " << max << " auf ihre Primeigenschaft hin zu überprüfen: "
+         << end - start
          << endl;
+
+    cout << "Primzahlen gefunden: " << primes << endl;
 
     return 0;
 }
